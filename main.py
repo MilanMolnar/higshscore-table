bedff1cfacff69b8901dac76bc7ffb8ca597d724
Milan Molnar <molnarmilan.cc@gmail.com>
2021-05-14 19:09:45 +0200

    drift

--- main.py
+++ main.py
@@ -21,6 +21,25 @@ def load():
         a = list(reader)
     print(a)
     return a
+
+def save_drift(list):
+    data = ""
+    for k,v in list.items():
+        data += v + ","
+    data_res = data[:-1]
+    print(data_res)
+    f = open("save_drift.csv", "a")
+    f.write(data_res + "\n")
+    f.close()
+    return None
+
+def load_drift():
+    with open("save_drift.csv", "r") as f:
+        reader = csv.DictReader(f)
+        a = list(reader)
+    print(a)
+    return a
+
      
 
 def format_view(list):
@@ -40,6 +59,18 @@ def highscores():
         return "None"
     if request.method == "GET":
         return format_view(load())
+
+
+@app.route("/highscores_drift", methods=["GET", "POST", "PUT"])
+def highscores_drift():
+    if request.method == "POST":
+        req_data = request.data
+        if req_data != None:
+            content = json.loads(req_data.decode("utf-8").replace("'",'"'))
+            save_drift(content)
+        return "None"
+    if request.method == "GET":
+        return format_view(load_drift())
 
 
 if __name__ == "__main__":
