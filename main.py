f9bab78fb6256d0fffdf0e7466b927d389ad0623
Milan Molnar <molnarmilan.cc@gmail.com>
2021-05-14 15:20:10 +0200

    save and load

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from flask import Flask, request, jsonify
 import json
+import csv  
 
 app = Flask(__name__)
 
-list_of_scores = []
+def save(list):
+    data = ""
+    for k,v in list.items():
+        data += v + ","
+    data_res = data[:-1]
+    print(data_res)
+    f = open("save.csv", "a")
+    f.write(data_res + "\n")
+    f.close()
+    return None
+
+def load():
+    with open("save.csv", "r") as f:
+        reader = csv.DictReader(f)
+        a = list(reader)
+    print(a)
+    return a
+     
 
 def format_view(list):
     view = ""
@@ -18,12 +36,10 @@ def highscores():
         req_data = request.data
         if req_data != None:
             content = json.loads(req_data.decode("utf-8").replace("'",'"'))
-            list_of_scores.append(content)
-    if len(list_of_scores) > 0:
-        list_view = format_view(list_of_scores)
-    else:
-        list_view = "no record yet :c"
-    return list_view
+            save(content)
+        return "None"
+    if request.method == "GET":
+        return format_view(load())
 
 
 if __name__ == "__main__":
