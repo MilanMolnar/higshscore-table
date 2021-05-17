44ed58a6c250bf4852b27062d82c2a330b8e8c9e
Milan Molnar <molnarmilan.cc@gmail.com>
2021-05-17 12:20:06 +0200

    lik

--- main.py
+++ main.py
@@ -40,14 +40,40 @@ def load_drift():
     print(a)
     return a
 
-     
+
+@app.route("/", methods=["GET"])     
+def index():
+    return "<div> \
+        <a href='http://127.0.0.1:4000/highscores' style='display:block; margin:20px; width: 115px;height:25px; background: lightcoral; padding: 10px; text-align: center; border-radius: 5px; color: white; font-weight: bold; line-height:25px; border: 1px solid black'>Dawn Sprint</a> \
+        <a href='http://127.0.0.1:4000/highscores_drift' style='margin: 20px; display:block; width: 115px;height:25px; background: lightblue; padding: 10px; text-align: center; border-radius: 5px; color: white; font-weight: bold; line-height:25px; border: 1px solid black'>Drift</a> \
+    </div>"
+
+
 
 def format_view(list):
     view = ""
-    laps = sorted(list, key = lambda i: i['finish_time_raw'])
-    for lap in laps:
-        view +="| Driver: " + lap["driver_name"] + " | Lap Time: " + lap["finish_time"] + " | Map: " + lap["map_name"] + " |<br>"
-    return view
+    list_top = "<h1 style='font: size 20;text-align:center; font-family: Arial;'>Highscores</h1>"+"<ul>"
+    #view +="| Driver: " + lap["driver_name"] + " | Lap Time: " + lap["finish_time"] + " | Map: " + lap["map_name"] + " |<br>"
+    list.sort(key=lambda item: float(item.get("finish_time_raw")))
+    #laps = sorted(list, key = lambda i: i('finish_time_raw'))
+    for lap in list:
+        if str(list.index(lap) + 1) != "1":
+            view += "<li style='text-align: center; list-style-type: none;margin-left: calc(50% - 230px);'>" \
+                        "<div style='background-color:lightblue;width: 400px; height: 60px; border-radius:6px; border: 1px solid black; margin-top: 20px'>" \
+                            "<div style=' background-color:lightblue; margin-top: 18px; margin-left:15px;  float:left;font-size:20;font-family: Arial'>" + str(list.index(lap) + 1) + ". "  + lap["driver_name"] + "</div>" \
+                            "<div style=' background-color:lightcoral ;margin-top: 18px;margin-right: 15px; float:right ;font-size:20;font-family: Arial;'>" + lap["finish_time"] + "</div>" \
+                        "</div>" \
+                    "</li>"
+        else:
+            view += "<li style='text-align: center; list-style-type: none;margin-left: calc(50% - 230px);'>" \
+                        "<div style='background-color:lightcoral;width: 400px; height: 60px; border-radius:6px; border: 1px solid black; margin-top: 20px'>" \
+                            "<div style=' background-color:lightblue; margin-top: 18px; margin-left:15px;  float:left;font-size:20;font-family: Arial'>" + str(list.index(lap) + 1) + ". "  + lap["driver_name"] + "</div>" \
+                            "<div style=' background-color:lightcoral ;margin-top: 18px;margin-right: 15px; float:right ;font-size:20;font-family: Arial;'>" + lap["finish_time"] + "</div>" \
+                        "</div>" \
+                    "</li>"
+
+    list_bot = "</ul>"
+    return list_top + view + list_bot
 
 @app.route("/highscores", methods=["GET", "POST", "PUT"])
 def highscores():
@@ -74,4 +100,4 @@ def highscores_drift():
 
 
 if __name__ == "__main__":
-    app.run(port=4996)
+    app.run(port=4000)
